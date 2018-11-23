8927e9c68524fd2bb23bced1a070406a581cdf2c
Gabriel Mihai Anghel <mihai.anghel@ibm.com>
2018-11-23 22:21:02 +0200

    - finished to return all events for a given sport

--- server/handlers/sportHandler.js
+++ server/handlers/sportHandler.js
@@ -19,7 +19,29 @@ module.exports = {
     },
 
     listAllEventsForGivenSport: async (req, res) => {
+        if (!req.params.givenSport) {
+            return res.sendStatus(400);
+        }
 
+        try {
+            let response = await getData(req.lang);
+            delete response.version;
+
+            response = response.sports.filter(item => {
+                return item.title.toLowerCase() === req.params.givenSport.toLowerCase()
+            });
+
+            if (response.length > 0) {
+                response = response[0].events.map(event => {
+                    delete event.outcomes
+                    return event;
+                });
+                return res.send(response);
+            }
+            res.send();
+        } catch (error) {
+            throw error;
+        }
     },
 
     listAllDataForGivenEvent: async (req, res) => {
